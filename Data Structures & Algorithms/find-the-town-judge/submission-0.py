570f76e143af1d5e285cd189bf65a624f9089c23
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-06-11 07:45:38 -0700

    Add: find-the-town-judge - submission-0

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        score = [0]*(n+1)
+        for a,b in trust:
+            score[a]-=1
+            score[b]+=1
+        
+        print(score)
+        for i in range(n+1):
+            if score[i] == n-1:
+                return i
+        return -1
