c6f6dede89b33a14aca93a03995ef15d789571f7
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-06-22 10:13:11 -0700

    Add: permutations-ii - submission-0

---
+++ Data Structures & Algorithms/permutations-ii/submission-0.py
@@ -0,0 +1,62 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        res = []
+        path = []
+
+        # used[i] tells us whether nums[i]
+        # is already placed into the current permutation
+        used = [False] * n
+
+        def dfs():
+
+            if len(path) == n:
+                res.append(path[:])
+                return
+
+            # =====================================
+            # TRY EVERY UNUSED NUMBER
+            # =====================================
+
+            for i in range(n):
+
+                # If this number is already used
+                # in the current permutation,
+                # skip it.
+                if used[i]:
+                    continue
+
+                # =================================
+                # CHOOSE nums[i]
+                # =================================
+                # duplicate handling
+                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                    continue
+
+                # Mark it as used
+                used[i] = True
+
+                # Add number to current permutation
+                path.append(nums[i])
+
+                # Explore all permutations
+                # that start with the current path.
+                dfs()
+
+                # =================================
+                # BACKTRACK
+                # =================================
+
+                # Remove the number we just added.
+                path.pop()
+
+                # Mark it unused again.
+                #
+                # This allows future branches
+                # to use this number.
+                used[i] = False
+
+        dfs()
+
+        return res
