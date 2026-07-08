f2820d69396cb7dee27dcc009407154c598ae683
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-07-08 13:10:57 -0700

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        seen = set()
+        l = 0
+        for r in range(len(nums)):
+            if len(seen) > k:
+                seen.remove(nums[l])
+                l += 1
+
+            while nums[r] in seen:
+                return True
+            seen.add(nums[r])
+ 
+        return False
+        
