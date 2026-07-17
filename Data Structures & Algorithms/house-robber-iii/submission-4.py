6aa00af052de309116f61c26dda6a4555cf6e4ec
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-07-17 07:40:22 -0700

    Add: house-robber-iii - submission-4

---
+++ Data Structures & Algorithms/house-robber-iii/submission-4.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+
+        def dfs(node):
+
+            if not node:
+                return (0, 0)
+
+            left_rob_node, left_skip_node = dfs(node.left)
+            right_rob_node, right_skip_node = dfs(node.right)
+
+            rob_node = node.val + left_skip_node + right_skip_node
+            skip_node = max(left_rob_node, left_skip_node) + max(right_rob_node, right_skip_node)
+
+            return (rob_node, skip_node)
+
+        return max(dfs(root))
+
+    # def maxAverage(self, root: Optional[TreeNode]) -> int:
+    #     ans = float('-inf')
+    
+    #     def dfs(node):
+
+    #         nonlocal ans
+
+    #         if not node:
+    #             return (0, 0)
+
+    #         left_sum_nodes, left_count_nodes = dfs(node.left)
+    #         right_sum_nodes, right_count_nodes = dfs(node.right)
+
+    #         sum_nodes += node.val + left_sum_nodes + right_sum_nodes
+    #         count_nodes += 1 + left_count_nodes + right_count_nodes
+
+    #         ans = max(ans, sum_nodes/count_nodes)
+
+    #         return (sum_nodes, count_nodes)
+
+    #     dfs(root)
+    #     return ans
+
+    
+        
+        
