fa219889604c6c1b11fc82bf8a4b310c817fd9d4
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-08-01 12:35:31 -0700

    Add: two-city-scheduling - submission-0

---
+++ Data Structures & Algorithms/two-city-scheduling/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        # Send exactly half people to city A and half to city B.
+        # Each person: [A cost, B cost]
+        # Calculate: difference = A - B
+        # Sort. People who benefit most from A go first.
+        # Time: O(nlogg) Space:O(1)
+        
+        costs.sort( key=lambda x:x[0]-x[1])
+
+        print(costs)
+        n = len(costs)//2
+
+        ans = 0
+
+        for i in range(len(costs)):
+
+            if i < n:
+                ans += costs[i][0]
+            else:
+                ans += costs[i][1]
+
+        return ans
+        
+
+        
