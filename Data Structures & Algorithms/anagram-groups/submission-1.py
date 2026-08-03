84dcf8f6dacc18bc495958634b5ce2cb0ea9fdd3
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-08-03 12:55:09 -0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # initialize a hashmap
+        # for loop strs
+        # initialize a list [0]*26
+        #  for loop str in strs
+        #  increase list element by ord of ch
+        # if list not in hashmap, store list as a key, value as [str]
+        # else hashmap key append str
+        # return all values in hashmap in a list
+        # Time:O(n*m) n len(strs) m len(str)
+        # Space: O(n*m)
+
+        freq = {}
+        
+        for s in strs:
+            count = [0] * 26
+            for ch in s:
+                count[ord(ch) - ord('a')] += 1
+
+            if tuple(count) not in freq:
+                freq[tuple(count)] = [s]
+            else:
+                freq[tuple(count)].append(s)
+        
+        return [ val for val in freq.values()]
+
+
+
+
+
+
+        
+
+        
