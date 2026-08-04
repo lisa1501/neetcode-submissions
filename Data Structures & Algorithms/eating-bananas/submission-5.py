034b59aa99f74a42c96ef2b11c25b1fe04a2039b
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-08-04 11:58:03 -0700

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # koko eats banana minimum speed is 1，max is max(piles)
+        # binary serach, mid 
+        # helper func, check if koko can eat all piles in h hours speed is mid
+        # if can, move max speed to mid
+        # if can not, move min speed to mid + 1
+        # time: O(n*logm) n: len(piles), m: max(piles) space: O(1)
+
+        def canFinish(target):
+            hour = 0
+            for p in piles:
+                hour += math.ceil(p/target)
+
+                if hour > h:
+                    return False
+            return hour <= h
+
+        lo = 1
+        hi = max(piles)
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if canFinish(mid):
+                hi = mid
+            else:
+                lo = mid + 1
+        return lo
+
+
+        
+
+
+        
+            
